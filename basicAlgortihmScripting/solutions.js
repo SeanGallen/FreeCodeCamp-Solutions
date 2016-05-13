1ab699c45bdce053085cf745a45d456e28bed160
Sean <sjgallen@gmail.com>
2016-05-12 19:08:36 -0500

    finished basicAlgortihm

--- basicAlgortihmScripting/solutions.js
+++ basicAlgortihmScripting/solutions.js
@@ -77,4 +77,117 @@ function confirmEnding(str, target) {
 
 confirmEnding("Bastian", "n");
 
+Truncate a string
+
+function truncateString(str, num) {
+  if (num < 4){
+    return str.slice(0, -(str.length-(num))) + "...";
+
+  } else if (str.length > num) {
+    return str.slice(0, -(str.length-(num-3))) + "...";
+  }
+  return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
+Chunky Monkey
+
+function chunkArrayInGroups(arr, size) {
+  result = [];
+
+  for(i = 0; i<arr.length; i = i + size) {
+    result.push(arr.slice(i, i+ size));
+  }
+  return result;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+Slasher Flick
+
+function slasher(arr, howMany) {
+  return arr.slice(howMany);
+}
+
+slasher([1, 2, 3], 2);
+
+Mutations
+
+function mutation(arr) {
+  first = arr[0].toLowerCase();
+  second = arr[1].toLowerCase().split('');
+  for(i = 0; i<second.length; i++) {
+    if (first.indexOf(second[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+mutation(["hello", "hey"]);
+
+Where do I belong
+
+function getIndexToIns(arr, num) {
+  arr = arr.sort();
+  count = 0;
+  for(i = 0; i <arr.length; i++){
+    if (arr[i] < num) {
+      count ++;
+    }
+  }
+
+  return count;
+}
+
+getIndexToIns([40, 60], 50);
+
+Return Largest Numbers in Array
+
+function largestOfFour(arr) {
+  result = [];
+  for (i=0; i<arr.length; i++){
+
+    max = Math.max(...arr[i]);
+    result.push(max);
+
+  }
+
+  return result;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+Falsy Bouncer
+
+var bouncer =  function(arr) {return arr.filter(val => val);};
+
+Caesars Cipher
+
+function rot13(str) { // LBH QVQ VG!
+  var result = "";
+  var char_code = 0;
+
+  for(var i=0; i<str.length; i++) {
+    if (!str[i].match(/^[A-Z]/)) {
+      result += str[i];
+    } else {
+
+      char_code = str.charCodeAt(i);
+      if ((char_code > 77) && (char_code < 91)) {
+
+        result += String.fromCharCode(char_code - 13);
+
+      } else if (char_code < 78) {
+
+        result += String.fromCharCode(char_code + 13);
+      }
+    }
+  }
+
+  return result;
+}
+
+rot13("SERR PBQR PNZC");
 
